63bae394eba83548e1fceb4241f011fad86467f6
Outpel/Wespel <80918551+fquisel@users.noreply.github.com>
2023-03-05 03:27:06 +0000

    drop function train

--- web-app-flask-covid-19-cnn/app.py
+++ web-app-flask-covid-19-cnn/app.py
@@ -11,20 +11,6 @@ img_size = 150
 
 model = load_model('model_t1.h5')
 
-def carga_img(data_dir):
-    data = [] 
-    for label in labels: 
-        path = os.path.join(data_dir, label)
-        class_num = labels.index(label)
-        for img in os.listdir(path):
-            try:
-                img_arr = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
-                resized_arr = cv2.resize(img_arr, (img_size, img_size))
-                data.append([resized_arr, class_num])
-            except Exception as e:
-                print(e)
-    return np.array(data)
-
 def hacer_predicciones(img):
     img = np.reshape(img, (1, img_size, img_size, 1))
     pred = model.predict(img)
@@ -38,6 +24,7 @@ def main():
 def prediccion():
     img = request.files['img']
     img_dir = 'static/' + img.filename
+    img.save(img_dir)
     img_arr = cv2.imdecode(np.fromstring(img.read(), np.uint8), cv2.IMREAD_GRAYSCALE)
     resized_arr = cv2.resize(img_arr, (img_size, img_size))
     label = hacer_predicciones(resized_arr)
